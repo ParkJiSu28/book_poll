d5a61970a992ca915d250654bc083c2b66819cea
parkjisu <qkrwltn28@gmail.com>
2019-03-07 17:07:23 +0900

    req

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,10 +17,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*f=8u2vmwl2309r85(@0%ak5)t%orzhto%8=lb*kb(v$gv#45s'
-
+#SECRET_KEY = '*f=8u2vmwl2309r85(@0%ak5)t%orzhto%8=lb*kb(v$gv#45s'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 TEMPLATE_DEBUG = True
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,3 +119,6 @@ LOGGING = {
         },
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
